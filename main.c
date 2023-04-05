58def2b886e544393a65fb4cee19694f2b643e57
Jack <jackhasakeyboard@gmail.com>
2023-04-05 05:45:25 -0600

    Prefixed input with value generated by utility in standard library

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char hash = '#';
+
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	int no = atoi(argv[1]);
+
+	printf("%c%d\n", hash, no);
+
 	return 0;
 }
